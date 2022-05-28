ab2dbe10332bc19ffff689ed78303035214e1932
Jed <jedwalton98@gmail.com>
2022-05-28 19:48:35 +0100

    Update Menu.java

--- Simple Banking System/task/src/banking/Menu.java
+++ Simple Banking System/task/src/banking/Menu.java
@@ -14,7 +14,10 @@ public class Menu {
 
     public static void displayCustomerAccountLoggedInView() {
         System.out.println("1. Balance");
-        System.out.println("2. Log out");
+        System.out.println("2. Add income");
+        System.out.println("3. Do transfer");
+        System.out.println("4. Close account");
+        System.out.println("5. Log out");
         System.out.println("0. Exit");
     }
 
